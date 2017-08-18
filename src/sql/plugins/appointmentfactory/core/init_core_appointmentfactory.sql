4f3f7e0eb1e4f5f26d73a41ed587b07ea169410b
laurent payen <laurent.payen@gmail.com>
2017-08-18 11:29:03 +0200

    Different order in the application menu

--- src/sql/plugins/appointmentfactory/core/init_core_appointmentfactory.sql
+++ src/sql/plugins/appointmentfactory/core/init_core_appointmentfactory.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'APPOINTMENTFACTORY_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('APPOINTMENTFACTORY_MANAGEMENT','appointmentfactory.adminFeature.ManageAppointmentfactory.name',1,'jsp/admin/plugins/appointmentfactory/ManageAppointmentfactory.jsp','appointmentfactory.adminFeature.ManageAppointmentfactory.description',0,'appointment','APPLICATIONS',NULL,NULL,4);
+('APPOINTMENTFACTORY_MANAGEMENT','appointmentfactory.adminFeature.ManageAppointmentfactory.name',1,'jsp/admin/plugins/appointmentfactory/ManageAppointmentfactory.jsp','appointmentfactory.adminFeature.ManageAppointmentfactory.description',0,'appointment','APPLICATIONS',NULL,NULL,6);
 
 
 --
