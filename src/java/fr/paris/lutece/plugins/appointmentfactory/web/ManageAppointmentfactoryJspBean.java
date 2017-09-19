ce5a014bc901e7cf005ec5dd2d372f6ccc916e30
laurent payen <laurent.payen@gmail.com>
2017-09-19 13:58:02 +0200

    Rollback

--- src/java/fr/paris/lutece/plugins/appointmentfactory/web/ManageAppointmentfactoryJspBean.java
+++ src/java/fr/paris/lutece/plugins/appointmentfactory/web/ManageAppointmentfactoryJspBean.java
@@ -39,15 +39,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.apache.commons.lang3.StringUtils;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 import fr.paris.lutece.plugins.appointment.service.FormService;
 import fr.paris.lutece.plugins.appointment.service.FormTraderService;
@@ -71,196 +73,208 @@ import net.sf.json.JSONObject;
 /**
  * ManageAppointmentfactory JSP Bean abstract class for JSP Bean
  */
-@Controller(controllerJsp = "ManageAppointmentfactory.jsp", controllerPath = "jsp/admin/plugins/appointmentfactory/", right = ManageAppointmentfactoryJspBean.RIGHT_MANAGEAPPOINTMENTFACTORY)
-public class ManageAppointmentfactoryJspBean extends MVCAdminJspBean {
+@Controller( controllerJsp = "ManageAppointmentfactory.jsp", controllerPath = "jsp/admin/plugins/appointmentfactory/", right = ManageAppointmentfactoryJspBean.RIGHT_MANAGEAPPOINTMENTFACTORY )
+public class ManageAppointmentfactoryJspBean extends MVCAdminJspBean
+{
 
-	/**
-	 * UID
-	 */
-	private static final long serialVersionUID = -9044694273971530510L;
+    /**
+     * UID
+     */
+    private static final long serialVersionUID = -9044694273971530510L;
 
-	// Rights
-	public static final String RIGHT_MANAGEAPPOINTMENTFACTORY = "APPOINTMENTFACTORY_MANAGEMENT";
+    // Rights
+    public static final String RIGHT_MANAGEAPPOINTMENTFACTORY = "APPOINTMENTFACTORY_MANAGEMENT";
 
-	// Templates
-	private static final String TEMPLATE_MANAGE_APPOINTMENT_FACTORY = "/admin/plugins/appointmentfactory/manage_appointment_factory.html";
+    // Templates
+    private static final String TEMPLATE_MANAGE_APPOINTMENT_FACTORY = "/admin/plugins/appointmentfactory/manage_appointment_factory.html";
 
-	// Actions
-	private static final String ACTION_EXPORT_FORM = "exportForm";
+    // Actions
+    private static final String ACTION_EXPORT_FORM = "exportForm";
 
-	// Session keys
-	private static final String SESSION_CURRENT_PAGE_INDEX = "appointmentfactory.session.currentPageIndex";
-	private static final String SESSION_ITEMS_PER_PAGE = "appointmentfactory.session.itemsPerPage";
-	private static final String SESSION_ATTRIBUTE_APPOINTMENT_FACTORY = "appointmentfactory.session";
+    // Session keys
+    private static final String SESSION_CURRENT_PAGE_INDEX = "appointmentfactory.session.currentPageIndex";
+    private static final String SESSION_ITEMS_PER_PAGE = "appointmentfactory.session.itemsPerPage";
+    private static final String SESSION_ATTRIBUTE_APPOINTMENT_FACTORY = "appointmentfactory.session";
 
-	// Properties for page titles
-	private static final String PROPERTY_PAGE_TITLE_MANAGE_APPOINTMENT_FACTORY = "appointmentfactory.manage.appointmentfactory.title";
+    // Properties for page titles
+    private static final String PROPERTY_PAGE_TITLE_MANAGE_APPOINTMENT_FACTORY = "appointmentfactory.manage.appointmentfactory.title";
 
-	// Properties
-	private static final String PROPERTY_DEFAULT_LIST_SERVERS_PER_PAGE = "appointmentfactory.listServers.itemsPerPage";
+    // Properties
+    private static final String PROPERTY_DEFAULT_LIST_SERVERS_PER_PAGE = "appointmentfactory.listServers.itemsPerPage";
 
-	// Views
-	private static final String VIEW_MANAGE_APPOINTMENT_FACTORY = "manageAppointmentfactory";
+    // Views
+    private static final String VIEW_MANAGE_APPOINTMENT_FACTORY = "manageAppointmentfactory";
 
-	// Errors
-	private static final String ERROR_MESSAGE_NO_INSTANCE_CHECKED = "appointmentfactory.validation.export.noInstanceChecked";
+    // Errors
+    private static final String ERROR_MESSAGE_NO_INSTANCE_CHECKED = "appointmentfactory.validation.export.noInstanceChecked";
 
-	// Infos
-	private static final String INFO_MESSAGE_FORM_EXPORT_OK = "appointmentfactory.message.form.export.ok";
+    // Infos
+    private static final String INFO_MESSAGE_FORM_EXPORT_OK = "appointmentfactory.message.form.export.ok";
 
-	// Markers
-	private static final String MARK_LIST_FORMS = "listForms";
-	private static final String MARK_LIST_INSTANCES = "listInstances";
-	private static final String MARK_NB_ITEMS_PER_PAGE = "nb_items_per_page";
-	private static final String MARK_PAGINATOR = "paginator";
-	private static final String MARK_APPOINTMENT_FACTORY = "appointmentfactory";
+    // Markers
+    private static final String MARK_LIST_FORMS = "listForms";
+    private static final String MARK_LIST_INSTANCES = "listInstances";
+    private static final String MARK_NB_ITEMS_PER_PAGE = "nb_items_per_page";
+    private static final String MARK_PAGINATOR = "paginator";
+    private static final String MARK_APPOINTMENT_FACTORY = "appointmentfactory";
 
-	// Parameters
-	private static final String PARAMETER_ID_INSTANCES = "inst";
-	private static final String PARAMETER_ID_FORM = "idForm";
-	private static final String PARAMETER_PAGE_INDEX = "page_index";
+    // Parameters
+    private static final String PARAMETER_ID_INSTANCES = "inst";
+    private static final String PARAMETER_ID_FORM = "idForm";
+    private static final String PARAMETER_PAGE_INDEX = "page_index";
 
-	// Constants
-	private static final String DEFAULT_CURRENT_PAGE = "1";
+    // Constants
+    private static final String DEFAULT_CURRENT_PAGE = "1";
 
-	// Session variable to store working values
-	private int _nDefaultItemsPerPage;
+    // Session variable to store working values
+    private int _nDefaultItemsPerPage;
 
-	private static final String JSP_MANAGE_APPOINTMENT_FACTORY = "jsp/admin/plugins/appointmentfactory/ManageAppointmentfactory.jsp";
+    private static final String JSP_MANAGE_APPOINTMENT_FACTORY = "jsp/admin/plugins/appointmentfactory/ManageAppointmentfactory.jsp";
 
-	/**
-	 * Default constructor
-	 */
-	public ManageAppointmentfactoryJspBean() {
-		_nDefaultItemsPerPage = AppPropertiesService.getPropertyInt(PROPERTY_DEFAULT_LIST_SERVERS_PER_PAGE, 10);
-	}
+    /**
+     * Default constructor
+     */
+    public ManageAppointmentfactoryJspBean( )
+    {
+        _nDefaultItemsPerPage = AppPropertiesService.getPropertyInt( PROPERTY_DEFAULT_LIST_SERVERS_PER_PAGE, 10 );
+    }
 
-	/**
-	 * Get the page to export form to instances
-	 * 
-	 * @param request
-	 *            the request
-	 * @return The HTML content to display
-	 */
-	@View(value = VIEW_MANAGE_APPOINTMENT_FACTORY, defaultView = true)
-	public String getManageAppointmentFactory(HttpServletRequest request) {
-		String strCurrentPageIndex = Paginator.getPageIndex(request, Paginator.PARAMETER_PAGE_INDEX,
-				(String) request.getSession().getAttribute(SESSION_CURRENT_PAGE_INDEX));
-		if (StringUtils.isEmpty(strCurrentPageIndex)) {
-			strCurrentPageIndex = DEFAULT_CURRENT_PAGE;
-		}
-		AppointmentFactoryDTO appointmentFactoryDTO = (AppointmentFactoryDTO) request.getSession()
-				.getAttribute(SESSION_ATTRIBUTE_APPOINTMENT_FACTORY);
-		if (appointmentFactoryDTO == null) {
-			appointmentFactoryDTO = new AppointmentFactoryDTO();
-		}
-		request.getSession().setAttribute(SESSION_CURRENT_PAGE_INDEX, strCurrentPageIndex);
-		int nItemsPerPage = Paginator.getItemsPerPage(request, Paginator.PARAMETER_ITEMS_PER_PAGE,
-				getIntSessionAttribute(request.getSession(), SESSION_ITEMS_PER_PAGE), _nDefaultItemsPerPage);
-		request.getSession().setAttribute(SESSION_ITEMS_PER_PAGE, nItemsPerPage);
-		UrlItem url = new UrlItem(JSP_MANAGE_APPOINTMENT_FACTORY);
-		url.addParameter(MVCUtils.PARAMETER_VIEW, VIEW_MANAGE_APPOINTMENT_FACTORY);
-		String strUrl = url.getUrl();
-		HashMap<Integer, InstanceDTO> mapInstances = InstanceService.getInstancesFromProperties();
-		appointmentFactoryDTO.setInstances(mapInstances);
-		request.getSession().setAttribute(SESSION_ATTRIBUTE_APPOINTMENT_FACTORY, appointmentFactoryDTO);
-		LocalizedPaginator<InstanceDTO> paginator = new LocalizedPaginator<InstanceDTO>(
-				new ArrayList<>(mapInstances.values()), nItemsPerPage, strUrl, PARAMETER_PAGE_INDEX,
-				strCurrentPageIndex, getLocale());
-		Map<String, Object> model = getModel();
-		model.put(MARK_LIST_FORMS, FormService.findAllInReferenceList());
-		model.put(MARK_NB_ITEMS_PER_PAGE, Integer.toString(nItemsPerPage));
-		model.put(MARK_PAGINATOR, paginator);
-		model.put(MARK_LIST_INSTANCES, paginator.getPageItems());
-		model.put(MARK_APPOINTMENT_FACTORY, appointmentFactoryDTO);
-		return getPage(PROPERTY_PAGE_TITLE_MANAGE_APPOINTMENT_FACTORY, TEMPLATE_MANAGE_APPOINTMENT_FACTORY, model);
-	}
+    /**
+     * Get the page to export form to instances
+     * 
+     * @param request
+     *            the request
+     * @return The HTML content to display
+     */
+    @View( value = VIEW_MANAGE_APPOINTMENT_FACTORY, defaultView = true )
+    public String getManageAppointmentFactory( HttpServletRequest request )
+    {
+        String strCurrentPageIndex = Paginator.getPageIndex( request, Paginator.PARAMETER_PAGE_INDEX,
+                (String) request.getSession( ).getAttribute( SESSION_CURRENT_PAGE_INDEX ) );
+        if ( StringUtils.isEmpty( strCurrentPageIndex ) )
+        {
+            strCurrentPageIndex = DEFAULT_CURRENT_PAGE;
+        }
+        AppointmentFactoryDTO appointmentFactoryDTO = (AppointmentFactoryDTO) request.getSession( ).getAttribute( SESSION_ATTRIBUTE_APPOINTMENT_FACTORY );
+        if ( appointmentFactoryDTO == null )
+        {
+            appointmentFactoryDTO = new AppointmentFactoryDTO( );
+        }
+        request.getSession( ).setAttribute( SESSION_CURRENT_PAGE_INDEX, strCurrentPageIndex );
+        int nItemsPerPage = Paginator.getItemsPerPage( request, Paginator.PARAMETER_ITEMS_PER_PAGE,
+                getIntSessionAttribute( request.getSession( ), SESSION_ITEMS_PER_PAGE ), _nDefaultItemsPerPage );
+        request.getSession( ).setAttribute( SESSION_ITEMS_PER_PAGE, nItemsPerPage );
+        UrlItem url = new UrlItem( JSP_MANAGE_APPOINTMENT_FACTORY );
+        url.addParameter( MVCUtils.PARAMETER_VIEW, VIEW_MANAGE_APPOINTMENT_FACTORY );
+        String strUrl = url.getUrl( );
+        HashMap<Integer, InstanceDTO> mapInstances = InstanceService.getInstancesFromProperties( );
+        appointmentFactoryDTO.setInstances( mapInstances );
+        request.getSession( ).setAttribute( SESSION_ATTRIBUTE_APPOINTMENT_FACTORY, appointmentFactoryDTO );
+        LocalizedPaginator<InstanceDTO> paginator = new LocalizedPaginator<InstanceDTO>( new ArrayList<>( mapInstances.values( ) ), nItemsPerPage, strUrl,
+                PARAMETER_PAGE_INDEX, strCurrentPageIndex, getLocale( ) );
+        Map<String, Object> model = getModel( );
+        model.put( MARK_LIST_FORMS, FormService.findAllInReferenceList( ) );
+        model.put( MARK_NB_ITEMS_PER_PAGE, Integer.toString( nItemsPerPage ) );
+        model.put( MARK_PAGINATOR, paginator );
+        model.put( MARK_LIST_INSTANCES, paginator.getPageItems( ) );
+        model.put( MARK_APPOINTMENT_FACTORY, appointmentFactoryDTO );
+        return getPage( PROPERTY_PAGE_TITLE_MANAGE_APPOINTMENT_FACTORY, TEMPLATE_MANAGE_APPOINTMENT_FACTORY, model );
+    }
 
-	/**
-	 * Export the form selected to the instances selected in json format
-	 * 
-	 * @param request
-	 * @return to the manage page
-	 * @throws AccessDeniedException
-	 */
-	@Action(ACTION_EXPORT_FORM)
-	public String doExportForm(HttpServletRequest request) throws AccessDeniedException {
-		AppointmentFactoryDTO appointmentFactoryDTO = (AppointmentFactoryDTO) request.getSession()
-				.getAttribute(SESSION_ATTRIBUTE_APPOINTMENT_FACTORY);
-		if (appointmentFactoryDTO == null) {
-			appointmentFactoryDTO = new AppointmentFactoryDTO();
-		}
-		String strIdForm = request.getParameter(PARAMETER_ID_FORM);
-		if (StringUtils.isNotEmpty(strIdForm)) {
-			appointmentFactoryDTO.setIdForm(Integer.parseInt(strIdForm));
-		}
-		request.getSession().setAttribute(SESSION_ATTRIBUTE_APPOINTMENT_FACTORY, appointmentFactoryDTO);
-		String[] tabIdInstances = request.getParameterValues(PARAMETER_ID_INSTANCES);
-		if (tabIdInstances == null || tabIdInstances.length == 0) {
-			addError(ERROR_MESSAGE_NO_INSTANCE_CHECKED, getLocale());
-			return redirectView(request, VIEW_MANAGE_APPOINTMENT_FACTORY);
-		}
-		if (callRestService(tabIdInstances, appointmentFactoryDTO.getInstances(),
-				FormTraderService.exportFormToJson(appointmentFactoryDTO.getIdForm()))) {
-			addInfo(INFO_MESSAGE_FORM_EXPORT_OK, getLocale());
-		}
-		return redirectView(request, VIEW_MANAGE_APPOINTMENT_FACTORY);
-	}
+    /**
+     * Export the form selected to the instances selected in json format
+     * 
+     * @param request
+     * @return to the manage page
+     * @throws AccessDeniedException
+     */
+    @Action( ACTION_EXPORT_FORM )
+    public String doExportForm( HttpServletRequest request ) throws AccessDeniedException
+    {
+        AppointmentFactoryDTO appointmentFactoryDTO = (AppointmentFactoryDTO) request.getSession( ).getAttribute( SESSION_ATTRIBUTE_APPOINTMENT_FACTORY );
+        if ( appointmentFactoryDTO == null )
+        {
+            appointmentFactoryDTO = new AppointmentFactoryDTO( );
+        }
+        String strIdForm = request.getParameter( PARAMETER_ID_FORM );
+        if ( StringUtils.isNotEmpty( strIdForm ) )
+        {
+            appointmentFactoryDTO.setIdForm( Integer.parseInt( strIdForm ) );
+        }
+        request.getSession( ).setAttribute( SESSION_ATTRIBUTE_APPOINTMENT_FACTORY, appointmentFactoryDTO );
+        String [ ] tabIdInstances = request.getParameterValues( PARAMETER_ID_INSTANCES );
+        if ( tabIdInstances == null || tabIdInstances.length == 0 )
+        {
+            addError( ERROR_MESSAGE_NO_INSTANCE_CHECKED, getLocale( ) );
+            return redirectView( request, VIEW_MANAGE_APPOINTMENT_FACTORY );
+        }
+        if ( callRestService( tabIdInstances, appointmentFactoryDTO.getInstances( ), FormTraderService.exportFormToJson( appointmentFactoryDTO.getIdForm( ) ) ) )
+        {
+            addInfo( INFO_MESSAGE_FORM_EXPORT_OK, getLocale( ) );
+        }
+        return redirectView( request, VIEW_MANAGE_APPOINTMENT_FACTORY );
+    }
 
-	/**
-	 * Call the rest service that will import the json file to the instances
-	 * selected
-	 * 
-	 * @param tabIdInstances
-	 *            the instances selected
-	 * @param mapInstances
-	 *            all the instances in the properties
-	 * @param jsonToExport
-	 *            the json to export
-	 * @return true if all the calls are ok
-	 */
-	private boolean callRestService(String[] tabIdInstances, HashMap<Integer, InstanceDTO> mapInstances,
-			JSONObject jsonToExport) {
-		boolean bCallOk = Boolean.TRUE;		
-		int nIdInstance;
-		InstanceDTO instanceDTO;		
-		Client client = Client.create();
-		StringBuilder stbPath = new StringBuilder().append(RestConstants.BASE_PATH).append(Constants.PLUGIN_PATH)
-				.append(Constants.FORM_PATH).append(Constants.IMPORT_PATH);
-		String strPath = stbPath.toString();
-		for (String strIdInstance : tabIdInstances) {
-			nIdInstance = Integer.parseInt(strIdInstance);
-			if (mapInstances.containsKey(nIdInstance)) {
-				instanceDTO = mapInstances.get(nIdInstance);
-				String url = instanceDTO.getUrl();
-				WebResource webResource = client.resource(url+strPath);
-				ClientResponse response = webResource.type(MediaType.APPLICATION_JSON).post(ClientResponse.class,
-						Entity.json( jsonToExport ));
-				if (response.getStatus() != Status.OK.getStatusCode()) {
-					addError("Error for instance " + instanceDTO.getName() + "; Response status = " + response.getStatus());
-					bCallOk = Boolean.FALSE;
-				}
-			}
-		}
-		return bCallOk;
-	}
+    /**
+     * Call the rest service that will import the json file to the instances selected
+     * 
+     * @param tabIdInstances
+     *            the instances selected
+     * @param mapInstances
+     *            all the instances in the properties
+     * @param jsonToExport
+     *            the json to export
+     * @return true if all the calls are ok
+     */
+    private boolean callRestService( String [ ] tabIdInstances, HashMap<Integer, InstanceDTO> mapInstances, JSONObject jsonToExport )
+    {
+        boolean bCallOk = Boolean.TRUE;
+        WebTarget target;
+        int nIdInstance;
+        InstanceDTO instanceDTO;
+        String url;
+        Response res;
 
-	/**
-	 * Get an integer attribute from the session
-	 * 
-	 * @param session
-	 *            The session
-	 * @param strSessionKey
-	 *            The session key of the item
-	 * @return The value of the attribute, or 0 if the key is not associated
-	 *         with any value
-	 */
-	private int getIntSessionAttribute(HttpSession session, String strSessionKey) {
-		Integer nAttr = (Integer) session.getAttribute(strSessionKey);
-		if (nAttr != null) {
-			return nAttr;
-		}
-		return 0;
-	}
+        Client client = ClientBuilder.newClient( ).register( JacksonJsonProvider.class );
+        StringBuilder stbPath = new StringBuilder( ).append( RestConstants.BASE_PATH ).append( Constants.PLUGIN_PATH ).append( Constants.FORM_PATH )
+                .append( Constants.IMPORT_PATH );
+        String strPath = stbPath.toString( );
+        for ( String strIdInstance : tabIdInstances )
+        {
+            nIdInstance = Integer.parseInt( strIdInstance );
+            if ( mapInstances.containsKey( nIdInstance ) )
+            {
+                instanceDTO = mapInstances.get( nIdInstance );
+                url = instanceDTO.getUrl( );
+                target = client.target( url ).path( strPath );
+                res = target.request( MediaType.APPLICATION_JSON ).post( Entity.json( jsonToExport ) );
+                if ( res.getStatus( ) != Status.OK.getStatusCode( ) )
+                {
+                    addError( "Error for instance " + instanceDTO.getName( ) + "; Response status = " + res.getStatus( ) );
+                    bCallOk = Boolean.FALSE;
+                }
+            }
+        }
+        return bCallOk;
+    }
+
+    /**
+     * Get an integer attribute from the session
+     * 
+     * @param session
+     *            The session
+     * @param strSessionKey
+     *            The session key of the item
+     * @return The value of the attribute, or 0 if the key is not associated with any value
+     */
+    private int getIntSessionAttribute( HttpSession session, String strSessionKey )
+    {
+        Integer nAttr = (Integer) session.getAttribute( strSessionKey );
+        if ( nAttr != null )
+        {
+            return nAttr;
+        }
+        return 0;
+    }
 
 }
